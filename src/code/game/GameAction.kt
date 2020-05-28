a905d3ebd49512d0c67f1c7738a4d66d3e7c5f59
Jean <jeanbarrossilva@outlook.com>
2020-05-28 15:41:54 -0400

    Prevent player from going off the canvas (again)

--- src/code/game/GameAction.kt
+++ src/code/game/GameAction.kt
@@ -5,10 +5,10 @@ import code.game.GameActor.*
 class GameAction(private val game: Game) {
 	fun move(player: Player, key: String) {
 		if (game.state is GameState.InProgress) {
-			if (key == "ArrowUp"    || key == "w" && player.y - 1 >= 0)                 --player.y
-			if (key == "ArrowLeft"  || key == "a" && player.x - 1 >= 0)                 --player.x
-			if (key == "ArrowRight" || key == "d" && player.x + 1 < game.canvas.width)  ++player.x
-			if (key == "ArrowDown"  || key == "s" && player.y + 1 < game.canvas.height) ++player.y
+			if ((key == "ArrowUp"    || key == "w") && player.y - 1 >= 0)                 --player.y
+			if ((key == "ArrowLeft"  || key == "a") && player.x - 1 >= 0)                 --player.x
+			if ((key == "ArrowRight" || key == "d") && player.x + 1 < game.canvas.width)  ++player.x
+			if ((key == "ArrowDown"  || key == "s") && player.y + 1 < game.canvas.height) ++player.y
 		}
 	}
 }
