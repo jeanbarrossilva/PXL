04f284f8d82e2a0c13b2ee720a78c46678b0403f
Jean <jeanbarrossilva@outlook.com>
2020-05-28 16:32:53 -0400

    Change code style to IDE default (again?)

--- src/code/game/GameActor.kt
+++ src/code/game/GameActor.kt
@@ -1,17 +1,17 @@
 package code.game
 
 sealed class GameActor(open val id: String, open var x: Double, open var y: Double) {
-	class Player(override val id: String, override var x: Double, override var y: Double) : GameActor(id, x, y) {
-		sealed class CollisionOccurrence {
-			data class Registered(val suspect: GameActor) : CollisionOccurrence()
-			object NonExistent : CollisionOccurrence()
-		}
-		
-		val collision = { suspects: List<GameActor> ->
-			suspects.find { it != this@Player && x == it.x && y == it.y }?.let { CollisionOccurrence.Registered(it) }
-				?: CollisionOccurrence.NonExistent
-		}
-	}
-	
-	data class Fruit(override val id: String, override var x: Double, override var y: Double) : GameActor(id, x, y)
+    class Player(override val id: String, override var x: Double, override var y: Double) : GameActor(id, x, y) {
+        sealed class CollisionOccurrence {
+            data class Registered(val suspect: GameActor) : CollisionOccurrence()
+            object NonExistent : CollisionOccurrence()
+        }
+        
+        val collision = { suspects: List<GameActor> ->
+            suspects.find { it != this@Player && x == it.x && y == it.y }?.let { CollisionOccurrence.Registered(it) }
+                ?: CollisionOccurrence.NonExistent
+        }
+    }
+    
+    data class Fruit(override val id: String, override var x: Double, override var y: Double) : GameActor(id, x, y)
 }
