c3019cf25e0f7fa4abb39e0a092ab1e97b0318bd
Jean <jeanbarrossilva@outlook.com>
2020-05-28 16:28:27 -0400

    Change code style to IDE default

--- src/code/game/Game.kt
+++ src/code/game/Game.kt
@@ -11,109 +11,109 @@ import kotlin.browser.window
 import kotlin.random.Random
 
 class Game(val canvas: HTMLCanvasElement, private val audio: HTMLAudioElement) {
-	internal var state: GameState = Waiting
-	private val action = GameAction(game = this)
-	
-	private val actors = mutableListOf<GameActor>()
-	
-	private val currentPlayer = Player("player1", x = 8.0, y = 8.0).apply player@{
-		window.addEventListener(
-			"keydown",
-			EventListener {
-				action.move(this@player, (it as KeyboardEvent).key)
-				
-				this@player.collision(actors).let { collision ->
-					if (collision is Registered) {
-						actors.remove(collision.suspect)
-						
-						run sound@{
-							val source = (document.createElement("source") as HTMLSourceElement).apply {
-								src = "src/coin.mp3"
-							}
-							
-							audio.apply {
-								preload = "auto"
-								currentTime = 0.1
-								
-								appendChild(source)
-								play()
-							}
-						}
-					}
-				}
-			},
-			true
-		)
-	}
-	
-	private fun addFruits() =
-		actors.apply {
-			fun generateRandomCoord(name: Char): Double? {
-				val comparable = when (name) {
-					'x' -> canvas.width
-					'y' -> canvas.height
-					else -> 0
-				}
-				
-				return try {
-					Random.nextInt(comparable).toDouble().let { generated ->
-						if (none { generated == (if (name == 'x') it.x else if (name == 'y') it.y else 0) })
-							generated
-						else {
-							generateRandomCoord(name)
-						}
-					}
-				} catch (e: Throwable) {
-					console.log("[game] Fruit generation is canceled until one of the existent ones is collected.")
-					null
-				}
-			}
-			
-			window.setInterval(
-				handler = generate@{
-					val (generatedX, generatedY) = generateRandomCoord('x') to generateRandomCoord('y')
-					
-					if (generatedX != null && generatedY != null) {
-						val fruit = Fruit(id = "fruit" + (lastIndex + 1), x = generatedX, y = generatedY)
-						add(fruit)
-					}
-				},
-				timeout = 5000
-			)
-		}
-	
-	private fun drawActors(): Unit =
-		with(canvas) {
-			(getContext("2d") as CanvasRenderingContext2D).apply {
-				clearRect(x = 0.0, y = 0.0, w = width.toDouble(), h = height.toDouble())
-				
-				actors.forEach { actor ->
-					fillStyle = when (actor) {
-						is Player -> if (actor == currentPlayer) "blue" else "gray"
-						is Fruit -> "red"
-					}
-					
-					fillRect(actor.x, actor.y, 1.0, 1.0)
-				}
-			}
-			
-			window.requestAnimationFrame { drawActors() }
-		}
-	
-	fun start() {
-		state = InProgress
-		actors.add(currentPlayer)
-		
-		addFruits()
-		drawActors()
-		
-		console.log("[game] Game started.")
-	}
-	
-	fun stop() {
-		state = Finished
-		actors.clear()
-		
-		console.log("[game] Game stopped.")
-	}
+    internal var state: GameState = Waiting
+    private val action = GameAction(game = this)
+    
+    private val actors = mutableListOf<GameActor>()
+    
+    private val currentPlayer = Player("player1", x = 8.0, y = 8.0).apply player@{
+        window.addEventListener(
+            "keydown",
+            EventListener {
+                action.move(this@player, (it as KeyboardEvent).key)
+                
+                this@player.collision(actors).let { collision ->
+                    if (collision is Registered) {
+                        actors.remove(collision.suspect)
+                        
+                        run sound@{
+                            val source = (document.createElement("source") as HTMLSourceElement).apply {
+                                src = "src/coin.mp3"
+                            }
+                            
+                            audio.apply {
+                                preload = "auto"
+                                currentTime = 0.1
+                                
+                                appendChild(source)
+                                play()
+                            }
+                        }
+                    }
+                }
+            },
+            true
+        )
+    }
+    
+    private fun addFruits() =
+        actors.apply {
+            fun generateRandomCoord(name: Char): Double? {
+                val comparable = when (name) {
+                    'x' -> canvas.width
+                    'y' -> canvas.height
+                    else -> 0
+                }
+                
+                return try {
+                    Random.nextInt(comparable).toDouble().let { generated ->
+                        if (none { generated == (if (name == 'x') it.x else if (name == 'y') it.y else 0) })
+                            generated
+                        else {
+                            generateRandomCoord(name)
+                        }
+                    }
+                } catch (e: Throwable) {
+                    console.log("[game] Fruit generation is canceled until one of the existent ones is collected.")
+                    null
+                }
+            }
+            
+            window.setInterval(
+                handler = generate@{
+                    val (generatedX, generatedY) = generateRandomCoord('x') to generateRandomCoord('y')
+                    
+                    if (generatedX != null && generatedY != null) {
+                        val fruit = Fruit(id = "fruit" + (lastIndex + 1), x = generatedX, y = generatedY)
+                        add(fruit)
+                    }
+                },
+                timeout = 5000
+            )
+        }
+    
+    private fun drawActors(): Unit =
+        with(canvas) {
+            (getContext("2d") as CanvasRenderingContext2D).apply {
+                clearRect(x = 0.0, y = 0.0, w = width.toDouble(), h = height.toDouble())
+                
+                actors.forEach { actor ->
+                    fillStyle = when (actor) {
+                        is Player -> if (actor == currentPlayer) "blue" else "gray"
+                        is Fruit -> "red"
+                    }
+                    
+                    fillRect(actor.x, actor.y, 1.0, 1.0)
+                }
+            }
+            
+            window.requestAnimationFrame { drawActors() }
+        }
+    
+    fun start() {
+        state = InProgress
+        actors.add(currentPlayer)
+        
+        addFruits()
+        drawActors()
+        
+        console.log("[game] Game started.")
+    }
+    
+    fun stop() {
+        state = Finished
+        actors.clear()
+        
+        console.log("[game] Game stopped.")
+    }
 }
